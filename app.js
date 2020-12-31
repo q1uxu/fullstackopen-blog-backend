c362e06aaa12757a6a6cc983f1dca9280060dcda
秦秋旭 <trolxyqin@outlook.com>
2020-12-31 12:12:22 +0800

    中间件

--- app.js
+++ app.js
@@ -9,16 +9,27 @@ const userRouter = require('./controller/user')
 const loginRouter = require('./controller/login')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
+const logger = require('./utils/logger');
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to mongodb successfully');
+  })
+  .catch (error => {
+    logger.error('error connecting to MONGODB', error.message);
+  });
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,19 @@
+const morgan = require('morgan');
+
+morgan.token('body', request => {
+  return JSON.stringify(request.body);
+});
+
+const requestLogger = morgan((tokens, request, response) => {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens['response-time'](request, response)+'ms',
+    tokens.body(request, response),
+  ].join(' ');
+});
+
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -6,8 +22,21 @@ const tokenExtractor = (request, response, next) => {
   next()
 };
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, request, response,) => {
+  logger.error(error.message);
+  return response.status(400).json({ error: error.message });
+};
+
 const middleware = {
-  tokenExtractor
-}
+  requestLogger,
+  tokenExtractor,
+  unknownEndpoint,
+  errorHandler
+};
+
 
 module.exports = middleware
