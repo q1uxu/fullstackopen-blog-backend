76052915a4cdc60459e7f1bbd15c1c26f3cc7c1d
秦秋旭 <trolxyqin@outlook.com>
2020-12-31 11:20:40 +0800

    4.18: bloglist expansion, 步骤6
    根据第4章节Token authentication实现基于令牌的认证。

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 require('express-async-errors')
 const app = express()
@@ -6,6 +7,7 @@ const mongoose = require('mongoose')
 mongoose.plugin(require('mongoose-unique-validator'));
 const blogRouter = require('./controller/blog')
 const userRouter = require('./controller/user')
+const loginRouter = require('./controller/login')
 
 const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -15,5 +17,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
